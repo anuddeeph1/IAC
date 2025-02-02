09bc58fff44b3929280bccf283e14b23becb66f7
Anudeep Nalla <121174814+anuddeeph1@users.noreply.github.com>
2025-02-02 20:40:57 +0530

    Update backend.tf

--- config-files/terraform/eks/backend.tf
+++ config-files/terraform/eks/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "eks-tf-backend-test"
     key    = "terraform/state.tfstate"
-    region = "us-west-"  # Replace with your region
+    region = var.region
   }
 }
